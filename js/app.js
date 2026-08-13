d7e3c97f445ea321df032e85791032d4b14f77e4
İsmail Türkoğlu <smlturkoglu@gmail.com>
2026-08-13 17:06:56 +0300

    feat: implement quiz question flow

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 const randomIndex = Math.floor(Math.random() * questionLists.length);
 const selectedQuestionList = questionLists[randomIndex];
+const getQuestionBtn = document.getElementById("get-question");
+const quiz = new Quiz(selectedQuestionList);
+
+getQuestionBtn.addEventListener("click", showQuestion);
+
+function showQuestion() {
+  if (quiz.questionIndex < quiz.questionList.length) {
+    console.log(quiz.getQuestion());
+  } else {
+    console.log("Quiz is over.");
+    console.log(quiz);
+  }
+}
