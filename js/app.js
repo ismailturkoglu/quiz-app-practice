b4b63cc8597cc64c356078eebee07042b0720f30
İsmail Türkoğlu <smlturkoglu@gmail.com>
2026-08-14 20:27:41 +0300

    feat: implement way one question rendering

--- js/app.js
+++ js/app.js
@@ -2,13 +2,12 @@ const randomIndex = Math.floor(Math.random() * questionLists.length);
 const selectedQuestionList = questionLists[randomIndex];
 const getQuestionBtn = document.getElementById("get-question");
 const quiz = new Quiz(selectedQuestionList);
+handleQuestion();
 
 getQuestionBtn.addEventListener("click", handleQuestion);
 
 function handleQuestion() {
-  logQuestion();
-  nextQuestion();
-  checkQuestionBtnState();
+  wayOne();
 }
 function isQuizOver() {
   return quiz.questionIndex >= quiz.questionList.length;
@@ -28,4 +27,43 @@ function checkQuestionBtnState() {
   if (isQuizOver()) {
     getQuestionBtn.disabled = true;
   }
+}
+function showQuestion() {
+  if (!isQuizOver()) {
+    /* <!-- HTML Structure -->
+    <div class="body">
+      <div class="question-text"></div>
+      <div class="question-options">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div> */
+
+    const question = quiz.getQuestion();
+    const options = question.options;
+    const optionsKeys = Object.keys(options);
+    const optionsValues = Object.values(options);
+
+    // div
+    const questionDiv = document.querySelector(".question-text");
+    questionDiv.textContent = question.questionText;
+
+    // ul
+    const optionsUl = document.querySelector(".question-options ul");
+
+    optionsUl.children[0].textContent = `${optionsKeys[0]}) ${optionsValues[0]}`;
+    optionsUl.children[1].textContent = `${optionsKeys[1]}) ${optionsValues[1]}`;
+    optionsUl.children[2].textContent = `${optionsKeys[2]}) ${optionsValues[2]}`;
+    optionsUl.children[3].textContent = `${optionsKeys[3]}) ${optionsValues[3]}`;
+  }
+}
+function wayOne() {
+  logQuestion();
+  showQuestion();
+  nextQuestion();
+  checkQuestionBtnState();
 }
