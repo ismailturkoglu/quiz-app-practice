07f638a6103dd06fe120943bacb830c015957ae2
İsmail Türkoğlu <smlturkoglu@gmail.com>
2026-08-14 15:39:53 +0300

    feat: add UI module for dynamic quiz content

---
+++ js/ui.js
@@ -0,0 +1,16 @@
+function UI() {}
+
+// /* dynamic part */
+// <div class="body">
+//   <div class="question-text">
+//     1-Hangisi Javascript paket yönetim uygulamasıdır?
+//   </div>
+//   <div class="question-options">
+//     <ul>
+//       <li>A. Node.js</li>
+//       <li>B. Typescript</li>
+//       <li>C. Nuget</li>
+//       <li>D. Npm</li>
+//     </ul>
+//   </div>
+// </div>
