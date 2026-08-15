24f0ab6eec77d152e80611b3bcd6d7a6e168cfe1
İsmail Türkoğlu <smlturkoglu@gmail.com>
2026-08-15 20:07:37 +0300

    feat: add UI module

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,83 @@
-function UI() {}
+function UI() {
+  this.questionBodyDiv = document.querySelector(".question-body");
+  this.getQuestionBtn = document.getElementById("get-question-btn");
+  this.questionDiv = document.querySelector(".question-text");
+  this.optionsUl = document.querySelector(".question-options ul");
+}
 
-// /* dynamic part */
-// <div class="body">
-//   <div class="question-text">
-//     1-Hangisi Javascript paket yönetim uygulamasıdır?
-//   </div>
-//   <div class="question-options">
-//     <ul>
-//       <li>A. Node.js</li>
-//       <li>B. Typescript</li>
-//       <li>C. Nuget</li>
-//       <li>D. Npm</li>
-//     </ul>
-//   </div>
-// </div>
+UI.prototype.createQuestion = function (question, questionIndex) {
+  /* 
+<!-- This HTML Structure will be createted -->
+<div class="body">
+  <div class="question-text"></div>
+  <div class="question-options">
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</div> 
+*/
+
+  this.questionBodyDiv.innerHTML = "";
+
+  const optionsList = Object.entries(question.options);
+
+  // div.body
+  const bodyDiv = document.createElement("div");
+  bodyDiv.classList.add("body");
+
+  // div.question-text
+  const questionTextDiv = document.createElement("div");
+  questionTextDiv.textContent = `${questionIndex + 1}) ${question.questionText}`;
+  questionTextDiv.classList.add("question-text");
+
+  // div.question-options
+  const questionOptionsDiv = document.createElement("div");
+  questionOptionsDiv.classList.add("question-options");
+
+  // ul
+  const ul = document.createElement("ul");
+
+  // li
+  for (let option of optionsList) {
+    const li = document.createElement("li");
+    // li'nin içeriği
+    li.textContent = `${option[0]}) ${option[1]}`;
+    ul.appendChild(li);
+  }
+
+  questionOptionsDiv.appendChild(ul);
+  bodyDiv.appendChild(questionTextDiv);
+  bodyDiv.appendChild(questionOptionsDiv);
+  this.questionBodyDiv.appendChild(bodyDiv);
+};
+
+UI.prototype.displayQuestion = function (question, questionIndex) {
+  /* <!-- This HTML Structure has to be in the index.html -->
+    <div class="body">
+      <div class="question-text"></div>
+      <div class="question-options">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div> */
+  const options = question.options;
+  const optionsKeys = Object.keys(options);
+  const optionsValues = Object.values(options);
+
+  // div
+  this.questionDiv.textContent = `${questionIndex + 1})  ${question.questionText}`;
+
+  // ul
+  this.optionsUl.children[0].textContent = `${optionsKeys[0]}) ${optionsValues[0]}`;
+  this.optionsUl.children[1].textContent = `${optionsKeys[1]}) ${optionsValues[1]}`;
+  this.optionsUl.children[2].textContent = `${optionsKeys[2]}) ${optionsValues[2]}`;
+  this.optionsUl.children[3].textContent = `${optionsKeys[3]}) ${optionsValues[3]}`;
+};
